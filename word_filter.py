8204021950f9d524140e5a7ea946e86e1da87553
aidencuneo <aidencuneo@gmail.com>
2024-04-19 20:10:38 +1000

    made checkBadMain much more concise and fixed error that may be caused by missing return
    + also added to the profanity list

--- word_filter.py
+++ word_filter.py
@@ -3,6 +3,7 @@ import string
 import util
 
 bad = open('profanity_wordlist.txt').read().lower().split('\n')
+chat_text = open('chat.txt', errors='ignore').read().lower().split('\n')
 
 
 def tok(text):
@@ -14,35 +15,19 @@ def is_bad(text):
     for w in tok(text.lower()):
         if w in bad:
             return True
+
     return False
 
 
-# result = is_bad(input('Enter text: '))
-# print('Sentence is ' + ('bad' if result else 'not bad'))
+def checkBadMain(body):
+    user_input = body['userInput']
+    res = {'message': 'your input does not contain bad words'}
 
-def checkBadMain(userInput):
-    # user_input = str(userInput)
-    user_input = userInput['userInput']
-    try:
-        if user_input == 'template':
-            good_word_list = []
-            chat_text_opener = open('chat.txt', errors='ignore').read().lower().split('\n')
-            for text in chat_text_opener[:200]:
-                if is_bad(text):
-                    # print('not bad:', text, end='\n\n')
-                    good_word_list.append(text)
+    if user_input == 'template':
+        bad_word_list = [line for line in chat_text[:200] if is_bad(line)]
+        res = {'bad_word_list': bad_word_list}
 
-            response_body = {"bad_word_list": good_word_list}
-            return util.build_response(200, response_body)
-        else:
-            for word in user_input.split():
-                if is_bad(word):
-                    return util.build_response(200, {
-                        "message": "your input contains bad words"
-                    })
-        
-    except KeyError as e:
-        missing_keys = str(e).strip("''")
-        error_message = f"Bad request - Missing '{missing_keys}' in the event"
-        print(error_message)
-        return util.build_response(400, {"message": error_message})
+    elif is_bad(user_input):
+        res = {'message': 'your input contains bad words'}
+
+    return util.build_response(200, res)
